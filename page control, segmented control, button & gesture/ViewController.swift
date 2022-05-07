41a918c493a19965560e4368eeb3dd2be7ec8280
Claire Pan <clairewww28@gmail.com>
2022-05-07 23:30:17 +0800

    Initial Commit

---
+++ page control, segmented control, button & gesture/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  page control, segmented control, button & gesture
+//
+//  Created by Wei Pan on 2022/5/7.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
